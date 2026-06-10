328f0d87d14523aace4d5a3694ac64ffd78a599c
ysus19py <jesusmateomedina19@gmail.com>
2026-06-10 20:31:06 -0300

    feat: implement cart API endpoints

---
+++ apps/cart/urls.py
@@ -0,0 +1,22 @@
+from rest_framework.routers import DefaultRouter
+
+
+from .views import CartViewSet, CartItemViewSet
+
+router = DefaultRouter()
+
+
+router.register(
+    r'carts',
+    CartViewSet,
+    basename='cart'
+)
+
+
+router.register(
+    r'cart-items',
+    CartItemViewSet,
+    basename='cart-item'
+)
+
+urlpatterns = router.urls
--- config/settings.py
+++ config/settings.py
@@ -37,8 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+
+    
     'apps.products',
     'apps.cart',
+    
 ]
 
 MIDDLEWARE = [

61513541d9a1f4f0cf9d5439f7cf39ca50d7887a
ysus19py <jesusmateomedina19@gmail.com>
2026-06-10 20:36:44 -0300

    feat: register cart models in admin panel

--- apps/cart/admin.py
+++ apps/cart/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Cart, CartItem
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'id',
+        'user',
+        'created_at'
+    )
+
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'id',
+        'cart',
+        'product_variant',
+        'quantity'
+    )

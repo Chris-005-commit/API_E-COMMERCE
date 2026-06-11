a13fb593cdb6b96051fc48a582eb0c6f20b749b9
LeticiaCaje <cajeleticia234@gmail.com>
2026-06-10 21:30:14 -0300

    Implementación de API REST para productos y categorías utilizando Django REST Framework

--- config/urls.py
+++ config/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('api/', include('apps.orders.urls')),
+    path('api/', include('apps.products.urls')),
 ]

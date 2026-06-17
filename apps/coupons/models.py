9767b09568f5b87ce795b8ac1e793f94eec95caf
ysus19py <jesusmateomedina19@gmail.com>
2026-06-17 20:17:55 -0300

    feat: create coupon model and app configuration

--- apps/coupons/models.py
+++ apps/coupons/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Coupon(models.Model):
+
+    # Código del cupón
+    code = models.CharField(
+        max_length=50,
+        unique=True
+    )
+
+    # Porcentaje de descuento
+    discount_percentage = models.PositiveIntegerField()
+
+    # Fecha de expiración
+    expiration_date = models.DateField()
+
+    # Estado del cupón
+    active = models.BooleanField(
+        default=True
+    )
+
+    def __str__(self):
+        return self.code

6fdeaf85a30893ab5bc5b9b47391dc3bc10f4007
LeticiaCaje <cajeleticia234@gmail.com>
2026-07-05 16:48:54 -0300

    feat: implementa ViewSet para cupones

--- apps/coupons/views.py
+++ apps/coupons/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Coupon
+from .serializers import CouponSerializer
+
+
+class CouponViewSet(viewsets.ModelViewSet):
+    queryset = Coupon.objects.all()
+    serializer_class = CouponSerializer

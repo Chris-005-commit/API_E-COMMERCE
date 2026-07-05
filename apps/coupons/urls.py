23e5be99ad881904e1935e521e28e453542de2e3
LeticiaCaje <cajeleticia234@gmail.com>
2026-07-05 16:51:04 -0300

    feat: configura rutas API para cupones

---
+++ apps/coupons/urls.py
@@ -0,0 +1,9 @@
+from rest_framework.routers import DefaultRouter
+
+from .views import CouponViewSet
+
+router = DefaultRouter()
+
+router.register(r'coupons', CouponViewSet)
+
+urlpatterns = router.urls

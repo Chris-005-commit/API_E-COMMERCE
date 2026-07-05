81032b33022a32438a230f9b604bb372eb3242d0
LeticiaCaje <cajeleticia234@gmail.com>
2026-07-05 17:22:11 -0300

    feat: implementa ViewSet para reseñas

--- apps/reviews/views.py
+++ apps/reviews/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Review
+from .serializers import ReviewSerializer
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
